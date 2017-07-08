2d738ef984fc707fccaa37b6dac93faf128e16b7
s952163 <s995110@yahoo.co.jp>
2017-07-08 18:38:07 +0900

    Task 12 Done

--- lesson1/task13/src/Task.kt
+++ lesson1/task13/src/Task.kt
@@ -1,3 +1,1 @@
-fun getList(): List<Int> {
-    return arrayListOf(1, 5, 2)//TODO("return the list sorted in descending order")
-}
+fun getList(): List<Int> = arrayListOf(1, 5, 2).sortedDescending()
