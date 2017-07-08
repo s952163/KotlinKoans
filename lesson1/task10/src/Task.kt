4d283b69913b2a7362414ec8c12e6a27f81e1c34
s952163 <s995110@yahoo.co.jp>
2017-07-08 16:15:48 +0900

    Task 11 Done

--- lesson1/task10/src/Task.kt
+++ lesson1/task10/src/Task.kt
@@ -2,3 +2,8 @@ fun Int.r(): RationalNumber = RationalNumber(this, denominator = 1)
 fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(numerator=this.first,denominator = this.second)
 
 data class RationalNumber(val numerator: Int, val denominator: Int)
+
+val x = 10.r()
+val x2 = Pair(10,20).r()
+
+
